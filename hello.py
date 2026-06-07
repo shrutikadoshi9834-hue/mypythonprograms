1c99abf71971e9b267bd040526ac3f0ec00a1449
shrutikadoshi9834-hue <shrutikadoshi9834@gmail.com>
2026-06-08 00:49:33 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello,github!")
+# A simple Python program to calculate age
+import datetime
+
+current_year = datetime.datetime.now().year
+birth_year = int(input("Enter your birth year: "))
+
+age = current_year - birth_year
+print(f"You are {age} years old!")
